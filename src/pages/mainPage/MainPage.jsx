de960ea237c000e38bc858cce39462f231657eb3
Dmytro Bezborodko <bezborodkodima12@gmail.com>
2023-12-06 20:34:39 +0200

    FEAT: Created Main Page

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,5 +1,50 @@
+import React from "react";
+import { useState } from "react";
+import trackList from "../../assets/trackList";
+import styles from "./mainPage.module.scss";
+import Track from "../../components/Track/Track";
+import { Input } from "@mui/material";
+
+const runSearch = (query) => {
+  if (!query) {
+    return trackList;
+  }
+  const loweCaseQuery = query.toLowerCase();
+
+  return (
+    trackList.filter((track) =>
+      track.title.toLowerCase().includes(loweCaseQuery)
+    ) ||
+    trackList.filter((track) =>
+      track.artists.toLowerCase().includes(loweCaseQuery)
+    )
+  );
+};
+
 const MainPage = () => {
-  return 123;
+  const [tracks, setTracks] = useState(trackList);
+
+  const handleChange = (e) => {
+    const foundTracks = runSearch(e.target.value);
+    setTracks(foundTracks);
+  };
+
+  return (
+    <div className={styles.search}>
+      <>
+        <Input
+          className={styles.input}
+          placeholder="Find your favorite track!"
+          onChange={handleChange}
+        />
+      </>
+      <div className={styles.list}>
+        {tracks.map((track) => (
+          <Track {...track} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
