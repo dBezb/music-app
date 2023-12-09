275a656fb77bd682a82e2507eefba696ea4affb1
Dmytro Bezborodko <bezborodkodima12@gmail.com>
2023-12-09 18:42:32 +0200

    FEAT: Added a PlayBar component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import MainPage from "./pages/mainPage/MainPage";
 import styles from "./global.module.scss";
+import PlayBar from "./components/PlayBar/PlayBar";
 
 function App() {
   return (
     <div className={styles.wrapper}>
       <MainPage />
+      <PlayBar />
     </div>
   );
 }
